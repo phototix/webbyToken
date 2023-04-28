3280bb4e10b5c6bd14fe3144435dfefbeb99acb5
Amanda <64722792+amandachong1224@users.noreply.github.com>
2023-04-28 16:34:55 +0800

    Create Start File

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Using the basic concept of blockchain
+Create a chains of tokens (block) as signature to carry forward in the network.
+*/
+
+/* Generic Token the first token to start the chain */
+$initialBlock = "":
+/* Here I use MD5 as the main HASH method. You can use your own here. */
+$initBlock = md5($initialBlock);
+
+/* Run this script to start the chain. */
+require("modules/start.php");
+?>
