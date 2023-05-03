524936f50448b94e76e3fafbd34f5f5993b39c18
AliceKarns <urbiswax.com@gmail.com>
2023-05-03 11:50:31 +0800

    Add decrypt and encrypt script

--- functions/encrypt.php
+++ functions/encrypt.php
@@ -4,4 +4,16 @@ Author: AliceKarns
 Version: 1.0
 Description: Encrypt.
 */
+
+if((isset($_POST["tokenKey"])&&isset($_POST["stringData"]))&&(!empty($_POST["tokenKey"])&&!empty($_POST["stringData"]))){
+
+	// Encrypt the data
+	$iv_size = openssl_cipher_iv_length('AES-256-CBC');
+	$iv = openssl_random_pseudo_bytes($iv_size);
+	$ciphertext = openssl_encrypt($_POST["stringData"], 'AES-256-CBC', $_POST["tokenKey"], OPENSSL_RAW_DATA, $iv);
+	$encrypted_data = base64_encode($iv . $ciphertext);
+
+	// Output decrytped key
+	echo $encrypted_data;
+}
 ?>
